343d28d2f20d07115bbfa4cc97e351e1243e427d
WJ-0928 <49092641+WJ-0928@users.noreply.github.com>
2021-07-05 08:35:27 +0800

    Add files via upload

--- 数据结构练习/2019-07-11-栈-队列/2019-07-11-栈-队列/2019-07-11-栈-队列/Stack.h
+++ 数据结构练习/2019-07-11-栈-队列/2019-07-11-栈-队列/2019-07-11-栈-队列/Stack.h
@@ -1,35 +1,37 @@
 #pragma once
 
-typedef struct Stack {
-	int	array[100];
-	//int capacity;
-	int top;	// size;
-}	Stack;
+typedef int SDataType;
 
-void StackInit(Stack *s) {
-	s->top = 0;
-}
 
-void StackPush(Stack *s, int val) {
-	s->array[s->top++];
-}
+/*
+静态栈：
+#define MAXSIZE 100
+typedef struct Stack
+{
+	SDataType array[MAXSIZE];
+	int top;
+}Stack;
 
-int StackPop(Stack *s) {
-	assert(s->top > 0);
+如果在笔试中要用到栈，自己可以快速定义一个静态栈，不考虑扩容
+*/
+typedef struct Stack
+{
+	SDataType* array;
+	int capacity;
+	int size;//栈中元素个数
+}Stack;
 
-	return s->array[--s->top];
-}
-
-int StackTop(Stack *s) {
-	assert(s->top > 0);
-
-	return s->array[s->top - 1];
-}
-
-int StackSize(Stack *s) {
-	return s->top;
-}
-
-int StackIsEmpty(Stack *s) {
-	return s->top == 0 ? 1 : 0;
-}
+void StackInit(Stack* ps);
+//入栈：尾插
+void StackPush(Stack* ps, SDataType data);
+//出栈：尾删
+void StackPop(Stack* ps);
+//获取栈顶元素
+SDataType StackTop(Stack* ps);
+//获取栈中有效元素个数
+int StackSize(Stack* ps);
+//检测栈是否为空
+int StackEmpty(Stack* ps);
+//销毁
+void StackDestroy(Stack* ps);
+void TestStack();
