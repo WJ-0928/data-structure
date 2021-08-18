c34dbb87a9df9ac62e82df36d01f199e10ad8073
WJ-0928 <49092641+WJ-0928@users.noreply.github.com>
2021-08-18 20:46:44 +0800

    Add files via upload

---
+++ 2019-06-29/2019-06-29-顺序表/2019-06-29-顺序表/BinaryTree.h
@@ -0,0 +1,32 @@
+#pragma once
+typedef int BTDataType;
+typedef struct BinaryTreeNode
+{
+	BTDataType data;
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+}BTNode;
+
+BTNode* CreateBinaryTree(int array[], int size, int index, int invalid);
+BTNode* CopyBinTree(BTNode* root);
+
+void BinaryTreeDestory(BTNode** root);
+int BinaryTreeSize(BTNode* root);
+int BinaryTreeLeafSize(BTNode* root);
+int BinaryTreeLevelKSize(BTNode* root, int k);
+BTNode* BinaryTreeFind(BTNode* root, BTDataType x);
+int GetHeight(BTNode* root);
+// 遍历
+void PrevOrder(BTNode* root);
+void InOrder(BTNode* root);
+void PostOrder(BTNode* root);
+// 非递归遍历
+// 层序遍历
+void BinaryTreeLevelOrder(BTNode* root);
+// 判断二叉树是否是完全二叉树
+int BinaryTreeComplete(BTNode* root);
+void BinaryTreePrevOrderNonR(BTNode* root);
+void BinaryTreeInOrderNonR(BTNode* root);
+void BinaryTreePostOrderNonR(BTNode* root);
+void TestBinaryTree();
+void TestBinTree();
