faa7fe4fa4fdabbe8aee1519fa9c6615aee0a371
WJ-0928 <49092641+WJ-0928@users.noreply.github.com>
2021-08-26 01:59:18 +0800

    Add files via upload

---
+++ 2019-06-29/2019-06-29-顺序表/2019-06-29-顺序表/main.cpp
@@ -0,0 +1,19 @@
+#include "Seqlist.h"
+#include"Stack.h"
+#include"Heap.h"
+#include"BinaryTree.h"
+#include"Sort.h"
+int main()
+{
+	/*
+	Cmp = Less;
+	Cmp(10, 20);
+
+	Cmp = Greater;
+	Cmp(10, 20);
+
+	TestHeap();
+	TestBinTree();*/
+	TestSort();
+	return 0;
+}
